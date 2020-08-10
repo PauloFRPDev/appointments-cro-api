4bca220abc3942cadfcff8f273c2a83c326a73c5
Paulo Felippe <paulofelipperp.dev@gmail.com>
2020-08-10 16:29:08 -0300

    feature: reset password

--- src/routes/password.routes.ts
+++ src/routes/password.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import SendForgotPasswordEmailService from '../services/SendForgotPasswordEmailService';
+import ResetPasswordService from '../services/ResetPasswordService';
 
-const usersRouter = Router();
+const passwordRouter = Router();
 
-usersRouter.post('/forgot', async (request, response) => {
+passwordRouter.post('/forgot', async (request, response) => {
   const { email } = request.body;
 
   const sendForgotPasswordEmailService = new SendForgotPasswordEmailService();
@@ -16,4 +17,17 @@ usersRouter.post('/forgot', async (request, response) => {
   return response.status(204).json();
 });
 
-export default usersRouter;
+passwordRouter.post('/reset', async (request, response) => {
+  const { token, password } = request.body;
+
+  const resetPassword = new ResetPasswordService();
+
+  await resetPassword.execute({
+    token,
+    password,
+  });
+
+  return response.status(204).json();
+});
+
+export default passwordRouter;
---
+++ src/services/ResetPasswordService.ts
@@ -0,0 +1,48 @@
+import { getCustomRepository } from 'typeorm';
+import { addHours, isAfter } from 'date-fns';
+
+import AppError from '../errors/AppError';
+
+import UsersRepository from '../repositories/UsersRepository';
+import UserTokensRepository from '../repositories/UserTokensRepository';
+
+import HashProvider from '../providers/HashProvider';
+
+interface Request {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: Request): Promise<void | null> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+
+    const hashProvider = new HashProvider();
+
+    const userToken = await userTokensRepository.findByToken(token);
+
+    if (!userToken) {
+      throw new AppError('User token does not exists');
+    }
+
+    const user = await usersRepository.findById(userToken?.user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    const tokenCreatedAt = userToken.created_at;
+    const compareDate = addHours(tokenCreatedAt, 2);
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('Token expired');
+    }
+
+    user.password = await hashProvider.generateHash(password);
+
+    await usersRepository.save(user);
+  }
+}
+
+export default ResetPasswordService;
