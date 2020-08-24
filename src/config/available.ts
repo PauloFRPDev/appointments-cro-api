c64ee3e15ac10ed78401398846598937d20490ad
Paulo Felippe <paulofelipperp.dev@gmail.com>
2020-08-24 13:09:15 -0300

    feat: library schedule

--- src/config/available.ts
+++ src/config/available.ts
@@ -15,12 +15,18 @@ export const AttendanceHour = [
 ] as TimeDTO[];
 
 export const LibraryHour = [
-  { hour: '09:00', appointmentQuantity: 2 },
-  { hour: '10:00', appointmentQuantity: 2 },
-  { hour: '11:00', appointmentQuantity: 2 },
-  { hour: '12:00', appointmentQuantity: 2 },
-  { hour: '13:00', appointmentQuantity: 2 },
+  { hour: '09:00', appointmentQuantity: 1 },
+  { hour: '09:30', appointmentQuantity: 1 },
+  { hour: '10:00', appointmentQuantity: 1 },
+  { hour: '10:30', appointmentQuantity: 1 },
+  { hour: '11:00', appointmentQuantity: 1 },
+  { hour: '11:30', appointmentQuantity: 1 },
+  { hour: '13:00', appointmentQuantity: 1 },
+  { hour: '13:30', appointmentQuantity: 1 },
   { hour: '14:00', appointmentQuantity: 1 },
-  { hour: '15:00', appointmentQuantity: 2 },
-  { hour: '16:00', appointmentQuantity: 2 },
+  { hour: '14:30', appointmentQuantity: 1 },
+  { hour: '15:00', appointmentQuantity: 1 },
+  { hour: '15:30', appointmentQuantity: 1 },
+  { hour: '16:00', appointmentQuantity: 1 },
+  { hour: '16:30', appointmentQuantity: 1 },
 ] as TimeDTO[];
