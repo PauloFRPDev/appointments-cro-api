c2e0dc710e7a3cf57d0c9690c44405e082b489fd
Paulo Felippe <paulofelipperp.dev@gmail.com>
2020-11-03 10:29:37 -0300

    update available times

--- src/config/available.ts
+++ src/config/available.ts
@@ -4,14 +4,14 @@ export interface TimeDTO {
 }
 
 export const AttendanceHour = [
-  { hour: '09:00', appointmentQuantity: 2 },
-  { hour: '10:00', appointmentQuantity: 2 },
-  { hour: '11:00', appointmentQuantity: 2 },
+  { hour: '09:00', appointmentQuantity: 3 },
+  { hour: '10:00', appointmentQuantity: 3 },
+  { hour: '11:00', appointmentQuantity: 3 },
   { hour: '12:00', appointmentQuantity: 1 },
-  { hour: '13:00', appointmentQuantity: 1 },
-  { hour: '14:00', appointmentQuantity: 2 },
-  { hour: '15:00', appointmentQuantity: 2 },
-  { hour: '16:00', appointmentQuantity: 2 },
+  { hour: '13:00', appointmentQuantity: 2 },
+  { hour: '14:00', appointmentQuantity: 3 },
+  { hour: '15:00', appointmentQuantity: 3 },
+  { hour: '16:00', appointmentQuantity: 3 },
 ] as TimeDTO[];
 
 export const LibraryHour = [
